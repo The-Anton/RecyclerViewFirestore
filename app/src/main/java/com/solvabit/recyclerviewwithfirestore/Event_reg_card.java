2f9142e0857fabf1723549f8c18b8a26e464035d
Shekhar <shekhar59324@gmail.com>
2020-04-12 10:05:20 +0530

    Created Data class for recyclerView

--- app/src/main/java/com/solvabit/recyclerviewwithfirestore/Event_reg_card.java
+++ app/src/main/java/com/solvabit/recyclerviewwithfirestore/Event_reg_card.java
@@ -2,5 +2,13 @@ package com.solvabit.recyclerviewwithfirestore;
 
 public class Event_reg_card {
 
+    String sub_event_name;
+    String prize;
+    boolean isSelected;
+
+    public Event_reg_card(String sub_event_name, String prize){
+        this.sub_event_name= sub_event_name;
+        this.prize= prize;
+    }
 
 }
