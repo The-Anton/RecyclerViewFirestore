6b9a860e738d5138ca46939bb9fc04ac5ec74db7
Shekhar <shekhar59324@gmail.com>
2020-04-12 10:09:56 +0530

    Created holder class for recyclerView

--- app/src/main/java/com/solvabit/recyclerviewwithfirestore/EventRegRecyclerViewHolder.java
+++ app/src/main/java/com/solvabit/recyclerviewwithfirestore/EventRegRecyclerViewHolder.java
@@ -16,13 +16,35 @@ import androidx.recyclerview.widget.RecyclerView;
 public class EventRegRecyclerViewHolder extends RecyclerView.ViewHolder {
 
 
-
+    public   TextView sub_event_name_text_view, prize_text_view;
+    public   CheckBox checkBox_bt;
 
 
     public EventRegRecyclerViewHolder(View itemView) {
         super(itemView);
 
 
+
+        /*checkBox_bt.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+
+                try
+                {
+                    // the String to int conversion happens here
+                    int prize =Integer.parseInt(prize_text_view.getText().toString().trim());
+                    // print out the value after the conversion
+                }
+                catch (NumberFormatException nfe)
+                {
+                    System.out.println("NumberFormatException: " + nfe.getMessage());
+                }
+
+
+            }
+        });*/
     }
 
 
