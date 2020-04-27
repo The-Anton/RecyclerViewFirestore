bdd68544bb120cf00bec2c71297280fbbe03e376
Shekhar <shekhar59324@gmail.com>
2020-04-27 21:07:22 +0530

    VideoPlayer setup in recyclerView code added - VideoPlayerRecyclerView.java

--- app/src/main/java/com/solvabit/recyclerviewwithfirestore/videoplayerRecyclerView/VideoPlayerRecyclerView.java
+++ app/src/main/java/com/solvabit/recyclerviewwithfirestore/videoplayerRecyclerView/VideoPlayerRecyclerView.java
@@ -228,5 +228,170 @@ public class VideoPlayerRecyclerView extends RecyclerView {
         }
 
 
+
+    public void playVideo(boolean isEndOfList) {
+
+        int targetPosition;
+
+        if(!isEndOfList){
+            int startPosition = ((LinearLayoutManager) getLayoutManager()).findFirstVisibleItemPosition();
+            int endPosition = ((LinearLayoutManager) getLayoutManager()).findLastVisibleItemPosition();
+
+            // if there is more than 2 list-items on the screen, set the difference to be 1
+            if (endPosition - startPosition > 1) {
+                endPosition = startPosition + 1;
+            }
+
+            // something is wrong. return.
+            if (startPosition < 0 || endPosition < 0) {
+                return;
+            }
+
+            // if there is more than 1 list-item on the screen
+            if (startPosition != endPosition) {
+                int startPositionVideoHeight = getVisibleVideoSurfaceHeight(startPosition);
+                int endPositionVideoHeight = getVisibleVideoSurfaceHeight(endPosition);
+
+                targetPosition = startPositionVideoHeight > endPositionVideoHeight ? startPosition : endPosition;
+            }
+            else {
+                targetPosition = startPosition;
+            }
+        }
+        else{
+            targetPosition = videoPlayerData.size() - 1;
+        }
+
+        Log.d(TAG, "playVideo: target position: " + targetPosition);
+
+        // video is already playing so return
+        if (targetPosition == playPosition) {
+            return;
+        }
+
+        // set the position of the list-item that is to be played
+        playPosition = targetPosition;
+        if (videoPlayerView == null) {
+            return;
+        }
+
+        // remove any old surface views from previously playing videos
+        videoPlayerView.setVisibility(INVISIBLE);
+        removeVideoView(videoPlayerView);
+
+        int currentPosition = targetPosition - ((LinearLayoutManager) getLayoutManager()).findFirstVisibleItemPosition();
+
+        View child = getChildAt(currentPosition);
+        if (child == null) {
+            return;
+        }
+
+        VideoPlayerAdapter holder = (VideoPlayerAdapter) child.getTag();
+        if (holder == null) {
+            playPosition = -1;
+            return;
+        }
+        thumbnail = holder.thumbnail;
+        progressBar = holder.progressBar;
+        volumeControl = holder.volumeControl;
+        viewHolderParent = holder.itemView;
+        requestManager = holder.requestManager;
+        frameLayout = holder.itemView.findViewById(R.id.media_container);
+
+        videoPlayerView.setPlayer(videoPlayer);
+
+        viewHolderParent.setOnClickListener(videoViewClickListener);
+
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(
+                context, Util.getUserAgent(context, "RecyclerView VideoPlayer"));
+        String mediaUrl = videoPlayerData.get(targetPosition).getMedia_url();
+        if (mediaUrl != null) {
+            MediaSource videoSource = new ExtractorMediaSource.Factory(dataSourceFactory)
+                    .createMediaSource(Uri.parse(mediaUrl));
+            videoPlayer.prepare(videoSource);
+            videoPlayer.setPlayWhenReady(true);
+        }
+    }
+
+    private OnClickListener videoViewClickListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            toggleVolume();
+        }
+    };
+
+    /**
+     * Returns the visible region of the video surface on the screen.
+     * if some is cut off, it will return less than the @videoSurfaceDefaultHeight
+     * @param playPosition
+     * @return
+     */
+    private int getVisibleVideoSurfaceHeight(int playPosition) {
+        int at = playPosition - ((LinearLayoutManager) getLayoutManager()).findFirstVisibleItemPosition();
+        Log.d(TAG, "getVisibleVideoSurfaceHeight: at: " + at);
+
+        View child = getChildAt(at);
+        if (child == null) {
+            return 0;
+        }
+
+        int[] location = new int[2];
+        child.getLocationInWindow(location);
+
+        if (location[1] < 0) {
+            return location[1] + videoSurfaceDefaultHeight;
+        } else {
+            return screenDefaultHeight - location[1];
+        }
+    }
+
+
+    // Remove the old player
+    private void removeVideoView(PlayerView videoView) {
+        ViewGroup parent = (ViewGroup) videoView.getParent();
+        if (parent == null) {
+            return;
+        }
+
+        int index = parent.indexOfChild(videoView);
+        if (index >= 0) {
+            parent.removeViewAt(index);
+            isVideoViewAdded = false;
+            viewHolderParent.setOnClickListener(null);
+        }
+
+    }
+
+    private void addVideoView(){
+        frameLayout.addView(videoPlayerView);
+        isVideoViewAdded = true;
+        videoPlayerView.requestFocus();
+        videoPlayerView.setVisibility(VISIBLE);
+        videoPlayerView.setAlpha(1);
+        thumbnail.setVisibility(GONE);
+    }
+
+    private void resetVideoView(){
+        if(isVideoViewAdded){
+            removeVideoView(videoPlayerView);
+            playPosition = -1;
+            videoPlayerView.setVisibility(INVISIBLE);
+            thumbnail.setVisibility(VISIBLE);
+        }
+    }
+
+    public void releasePlayer() {
+
+        if (videoPlayer != null) {
+            videoPlayer.release();
+            videoPlayer = null;
+        }
+
+        viewHolderParent = null;
+    }
+
+
+
+
     }
 
