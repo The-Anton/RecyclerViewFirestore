1377c40382765a27cb3cd83aecd5ad4d1481ef39
Shekhar <shekhar59324@gmail.com>
2020-04-27 15:20:05 +0530

    VideoPlayer ViewHolder class for custom recycler added

--- app/src/main/java/com/solvabit/recyclerviewwithfirestore/videoplayerRecyclerView/VideoPlayerHolder.java
+++ app/src/main/java/com/solvabit/recyclerviewwithfirestore/videoplayerRecyclerView/VideoPlayerHolder.java
@@ -1,24 +1,43 @@
 package com.solvabit.recyclerviewwithfirestore.videoplayerRecyclerView;
 
-import android.view.ViewGroup;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class VideoPlayerHolder extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-    @NonNull
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+import com.bumptech.glide.RequestManager;
+import com.solvabit.recyclerviewwithfirestore.R;
+
+public class VideoPlayerHolder  extends RecyclerView.ViewHolder {
+
+    FrameLayout media_container;
+    TextView title;
+    ImageView thumbnail, volumeControl;
+    ProgressBar progressBar;
+    View parent;
+    RequestManager requestManager;
+
+    public VideoPlayerHolder(@NonNull View itemView) {
+        super(itemView);
+        parent = itemView;
+        media_container = itemView.findViewById(R.id.media_container);
+        thumbnail = itemView.findViewById(R.id.thumbnail);
+        title = itemView.findViewById(R.id.title);
+        progressBar = itemView.findViewById(R.id.progressBar);
+        volumeControl = itemView.findViewById(R.id.volume_control);
     }
 
-    @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+    public void onBind(videoPlayerData videoPlayerData, RequestManager requestManager) {
+        this.requestManager = requestManager;
+        parent.setTag(this);
+        title.setText(videoPlayerData.getTitle());
+        this.requestManager
+                .load(videoPlayerData.getThumbnail())
+                .into(thumbnail);
     }
 
-    @Override
-    public int getItemCount() {
-        return 0;
-    }
 }
